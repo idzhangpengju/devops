e223bb97873b038f3f809e164b78c2537725a646
Administrator <admin@example.com>
2019-11-08 18:01:30 +0800

    add redis dockerfile

---
+++ docker-centos-redis/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# -------------------------------
+set -e
+# -------------------------------
+REDIS_FLAG=false
+REDIS_PATH='/opt/redis_5.0.5/bin'
+REDIS_COMMANDS="redis-cli \
+                redis-server"
+# -------------------------------
+for redis_cmd in ${REDIS_COMMANDS}
+do
+    if [[ ${1} == ${redis_cmd} ]]
+    then
+        REDIS_FLAG=true
+    fi
+done
+# -------------------------------
+if [[ ${REDIS_FLAG} == true ]]
+then
+    ${REDIS_PATH}/${@}
+else
+    echo '[ERROR]: No such Redis command...'
+    exit 1
+fi
+
---
+++ docker-centos-redis/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# ------------------------------
+REDIS_OPTIONS="--loglevel verbose \
+               --bind 0.0.0.0 \
+               --port 6379 \
+               --requirepass foobar123 \
+               --maxclients 10000 \
+               --timeout 0 \
+               --tcp-keepalive 300 \
+               --daemonize no \
+               --supervised no \
+               --pidfile none \
+               --databases 32 \
+               --always-show-logo yes \
+               --appendonly no \
+               --appendfsync no"
+# ------------------------------
+sudo docker run -d -i -t \
+                --env LC_ALL=en_US.UTF-8 \
+                --name redis \
+                -p 0.0.0.0:6379:6379 \
+                zhangpengju/centos:redis \
+                redis-server ${REDIS_OPTIONS}
+
